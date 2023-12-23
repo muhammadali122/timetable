f778b5f2c662123c1deca181d7ecfa1c3831a1fb
Muhammad Ali <56887100+muhammadali122@users.noreply.github.com>
2023-12-23 13:39:04 +0500

    Update wether.jsx

--- src/pages/wether.jsx
+++ src/pages/wether.jsx
@@ -1,7 +1,7 @@
 // Import modules
 
-import React, { useEffect, useState } from "react";
-import { Carousel } from "react-bootstrap";
+import React, { useState } from "react";
+import { Carousel, Spinner } from "react-bootstrap";
 import mist_icon from "../assets/mist.png";
 import rain_icon from "../assets/rain.png";
 import snow_icon from "../assets/snow.png";
@@ -11,14 +11,13 @@ import clouds_icon from "../assets/clouds.png";
 import search_icon from "../assets/search.png";
 import drizzle_icon from "../assets/drizzle.png";
 import humidity_icon from "../assets/humidity.png";
-import SearchField from "../components/searchField";
 import { getWetherData } from "../services/wether.service";
 <meta name="viewport" content="width=device-width, initial-scale=1" />;
 // Weather Api fetching
 
 const Weather = () => {
   const [index, setIndex] = useState(0);
-  const [searchSring, setSearchString] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const [weatherData, setWeatherData] = useState([
     {
       icon: "",
@@ -27,29 +26,22 @@ const Weather = () => {
       wind: { speed: 0, deg: 0 },
     },
   ]);
-  const [currentWeatherData, setCurrentWeatherData] = useState({
-    icon: "",
-    main: {},
-    name: "",
-    wind: { speed: 0, deg: 0 },
-  });
+  const [searchString, setSearchString] = useState("");
 
   const handleSelect = (selectedIndex) => {
-    console.log(
-      "🚀 ~ file: wether.jsx:33 ~ handleSelect ~ selectedIndex:",
-      selectedIndex
-    );
-    setIndex(selectedIndex);
-    setCurrentWeatherData(weatherData[selectedIndex]);
+    if (weatherData.length > 1) {
+      setIndex(selectedIndex);
+    }
   };
 
   //Handle get data
   const handleGetWeather = () => {
-    if (searchSring) {
-      getWetherData(searchSring).then(
+    if (searchString) {
+      setIsLoading(true);
+      getWetherData(searchString).then(
         (res) => {
           const { main, weather, name, wind } = res.data;
-          setWeatherData([
+          const obj = [
             ...weatherData,
             {
               main: main,
@@ -57,12 +49,14 @@ const Weather = () => {
               name: name,
               wind: wind,
             },
-          ]);
+          ];
+          setWeatherData(obj);
           setSearchString("");
+          setIsLoading(false);
         },
         (error) => {
-          setWeatherData({});
-          alert(error);
+          alert("No data Found");
+          setIsLoading(false);
         }
       );
     } else {
@@ -99,50 +93,75 @@ const Weather = () => {
     }
   };
 
+  const renderPreviousSearch = () => {
+    return (
+      weatherData.length > 0 &&
+      searchString.length > 0 &&
+      weatherData.map((val) =>
+        val.name.toLowerCase().includes(searchString.toLowerCase()) ? (
+          <p
+            onClick={() => setSearchString(val.name)}
+            className="text-start px-2 search-value"
+          >
+            {val.name}
+          </p>
+        ) : (
+          <></>
+        )
+      )
+    );
+  };
+
   return (
     <div className="container">
       <div className="top-bar d-flex">
-      <div>
-        <input
-          type="text"
-          value={searchSring}
-          className="cityInput px-2"
-          placeholder="Enter city name"
-          onChange={(e) => handleSearchValue(e.target.value)}
-        />
-        <div className="position">
-          {searchSring.length > 0 &&
-            weatherData.map((val) => (
-              <p
-                onClick={() => setSearchString(val.name)}
-                className="text-start px-2"
-              >
-                {val.name}
-              </p>
-            ))}
+        <div>
+          <input
+            type="text"
+            value={searchString}
+            className="cityInput px-2"
+            placeholder="Enter city name"
+            onChange={(e) => handleSearchValue(e.target.value)}
+          />
+          <div className="position">
+            {isLoading && (
+              <Spinner animation="border" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </Spinner>
+            )}
+            {renderPreviousSearch()}
+          </div>
+        </div>
+        <div className="search-icon" onClick={() => handleGetWeather()}>
+          <img src={search_icon} alt="" />
         </div>
       </div>
-      <div className="search-icon" onClick={() => handleGetWeather()}>
-        <img src={search_icon} alt="" />
-      </div>
-    </div>
 
       <Carousel activeIndex={index} onSelect={handleSelect}>
         <Carousel.Item>
           <div className="weather-image">
-            <img src={renderWetherIcon(currentWeatherData)} alt="" />
+            <img
+              src={renderWetherIcon(weatherData[weatherData.length - 1])}
+              alt=""
+            />
           </div>
           <div className="weather-temp">
-            {Math.round(currentWeatherData.main?.temp) || 16}°C
+            {Math.round(weatherData[weatherData.length - 1]?.main?.temp) || 16}
+            °C
           </div>
-          <div className="weather-location">{currentWeatherData.name}</div>
+          <div className="weather-location">
+            {weatherData[weatherData.length - 1]?.name || " City Name"}
+          </div>
 
           <div className="row">
             <div className="col-6">
               <img src={humidity_icon} alt="" className="icon" />
               <div className="data">
                 <div className="humidity-percentage">
-                  {Math.round(currentWeatherData.main?.humidity) || 82}%
+                  {Math.round(
+                    weatherData[weatherData.length - 1]?.main?.humidity
+                  ) || 82}
+                  %
                 </div>
                 <div className="text">Humidity</div>
               </div>
@@ -151,13 +170,54 @@ const Weather = () => {
               <img src={wind_icon} alt="" className="icon" />
               <div className="data">
                 <div className="wind-rate">
-                  {Math.round(currentWeatherData.wind?.speed) || 0} km/h
+                  {Math.round(
+                    weatherData[weatherData.length - 1]?.wind?.speed
+                  ) || 0}{" "}
+                  km/h
                 </div>
                 <div className="text">wind speed</div>
               </div>
             </div>
           </div>
         </Carousel.Item>
+        {weatherData.length > 0 ? (
+          weatherData.map((wethe) => (
+            <Carousel.Item>
+              <div className="weather-image">
+                <img src={renderWetherIcon(wethe)} alt="" />
+              </div>
+              <div className="weather-temp">
+                {Math.round(wethe?.main?.temp) || 16}°C
+              </div>
+              <div className="weather-location">
+                {wethe.name || " City Name"}
+              </div>
+
+              <div className="row">
+                <div className="col-6">
+                  <img src={humidity_icon} alt="" className="icon" />
+                  <div className="data">
+                    <div className="humidity-percentage">
+                      {Math.round(wethe.main?.humidity) || 82}%
+                    </div>
+                    <div className="text">Humidity</div>
+                  </div>
+                </div>
+                <div className="col-6">
+                  <img src={wind_icon} alt="" className="icon" />
+                  <div className="data">
+                    <div className="wind-rate">
+                      {Math.round(wethe.wind?.speed) || 0} km/h
+                    </div>
+                    <div className="text">wind speed</div>
+                  </div>
+                </div>
+              </div>
+            </Carousel.Item>
+          ))
+        ) : (
+          <></>
+        )}
       </Carousel>
     </div>
   );
